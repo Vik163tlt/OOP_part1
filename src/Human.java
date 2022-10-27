341fc922c06c40fe50a81c5d6c07d0431bc84fe5
VitaliyK <Vik163tlt@ya.ru>
2022-10-27 15:00:40 +0300

    На проверку!

---
+++ src/Human.java
@@ -0,0 +1,26 @@
+public class Human {
+
+    public String name;
+    public String town;
+    public int dateOfBirth;
+    public String job;
+
+    Human (String name, String town, int dateOfBirth, String job) {
+        if (name == null || name.isEmpty()) {
+            this.name = "!информация об имени не указана!";}
+        else {this.name = name;}
+        if (town == null || town.isEmpty()) {
+            this.town = "!информация об имени не указана!";}
+        else {this.town = town;}
+        if (dateOfBirth >= 0) {
+            this.dateOfBirth = dateOfBirth;}
+        else {this.dateOfBirth = Math.abs(dateOfBirth);}
+        if (job == null || job.isEmpty()) {
+            this.job = "!информация об имени не указана!";}
+        else {this.job = job;}
+    }
+
+    void hallo() {
+        System.out.println("Привет! Меня зовут "+ name +" Я из города "+ town +" Я родился в "+ dateOfBirth +" году. Я работаю на должности "+ job+". Будем знакомы!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Класс Human:\n");
+
+        Human maxim = new Human ("Максим","Минск", 35, "бренд-менеджер");
+        Human anna = new Human ("Анна", "Москва", 29,"методист образовательных программ");
+        Human kate = new Human ("Катя", "Калининград", 28, "продакт-менеджер");
+        Human artem = new Human ("Артем","Москва",-27, "директор по развитию бизнеса");
+
+        maxim.hallo();
+        anna.hallo();
+        kate.hallo();
+        artem.hallo();
+
+        System.out.println("\nКласс Car:\n");
+
+        Car ladaGranta = new Car ("Lada","Granta",1.7,"желтый",2015,"Россия");
+        Car audi_A8 = new Car ("Audi","A8 50L TDI quattro,",3.0,"черный",2020,"Германия");
+        Car bmw_Z8 = new Car ("BMW","Z8",3.0,"черный",2021,"Германия");
+        Car kiaSportage_4 = new Car ("Kia","Sportage 4-го поколения",2.4,"красный",2018,"Южная Корея");
+        Car hyundaiAvante  = new Car ("Hyundai","Avante",1.6 ,"оранжевый",2016,"Южная Корея");
+
+        ladaGranta.description();
+        audi_A8.description();
+        bmw_Z8.description();
+        kiaSportage_4.description();
+        hyundaiAvante.description();
+
     }
 }
