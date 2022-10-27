e8d31be616a83582287da9289ace073aa99ae9a7
VitaliyK <Vik163tlt@ya.ru>
2022-10-27 15:53:11 +0300

    На проверку!

--- src/Human.java
+++ src/Human.java
@@ -1,5 +1,7 @@
 public class Human {
 
+    int curentYear = 2022;
+
     public String name;
     public String town;
     public int dateOfBirth;
@@ -21,6 +23,6 @@ public class Human {
     }
 
     void hallo() {
-        System.out.println("Привет! Меня зовут "+ name +" Я из города "+ town +" Я родился в "+ dateOfBirth +" году. Я работаю на должности "+ job+". Будем знакомы!");
+        System.out.println("Привет! Меня зовут "+ name +" Я из города "+ town +" Я родился в "+( curentYear - dateOfBirth )+" году. Я работаю на должности "+ job+". Будем знакомы!");
     }
 }
